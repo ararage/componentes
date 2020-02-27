f1cc4b2944cffbaa37af98f5e95e24ab816e8433
José Ricardo Pérez <josericardo.perez.next@bbva.com>
2020-02-26 23:43:17 -0600

    Finished Cards Page of Video 75, next video 76

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -4,39 +4,61 @@ class CardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cards')
-      ),
+      appBar: AppBar(title: Text('Cards')),
       body: ListView(
         padding: EdgeInsets.all(10.0),
         children: <Widget>[
           _cardTipo1(),
-          SizedBox(),
+          SizedBox(
+            height: 30.0,
+          ),
           _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardTipo2(),
+          SizedBox(
+            height: 30.0,
+          ),
         ],
       ),
     );
   }
 
-  Widget _cardTipo1(){
+  Widget _cardTipo1() {
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       child: Column(
         children: <Widget>[
           ListTile(
             leading: Icon(Icons.photo_album, color: Colors.blue),
             title: Text('Soy el titulo de esta tarjeta'),
-            subtitle: Text('Chinguen a su madre todos los que vean este mensaje'),
+            subtitle:
+                Text('Chinguen a su madre todos los que vean este mensaje'),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               FlatButton(
                 child: Text('Cancelar'),
-                onPressed: (){},
+                onPressed: () {},
               ),
               FlatButton(
                 child: Text('Ok'),
-                onPressed: (){},
+                onPressed: () {},
               )
             ],
           )
@@ -46,23 +68,36 @@ class CardPage extends StatelessWidget {
   }
 
   Widget _cardTipo2() {
-    return Card(
+    final card = Container(
       child: Column(
         children: <Widget>[
           FadeInImage(
             image: NetworkImage(
-              'http://www.lawlor.me/wp-content/uploads/2014/05/1-Tulum-Beach-Mexico-Download.jpg'
-            ),
+                'http://www.lawlor.me/wp-content/uploads/2014/05/1-Tulum-Beach-Mexico-Download.jpg'),
             placeholder: AssetImage('assets/jar-loading.gif'),
             fadeInDuration: Duration(milliseconds: 200),
             height: 250.0,
             fit: BoxFit.cover,
           ),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: Text("Tulum")
-          )
+          Container(padding: EdgeInsets.all(10.0), child: Text("Tulum"))
         ],
+      ),
+    );
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Colors.black26,
+              blurRadius: 10.0,
+              spreadRadius: 2.0,
+              offset: Offset(2.0, 10.0)),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: card,
       ),
     );
   }
