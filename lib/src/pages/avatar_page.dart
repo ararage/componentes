739671b52d5ac955713ec1f5fb2bc8751ac3b607
José Ricardo Pérez <josericardo.perez.next@bbva.com>
2020-02-28 00:09:58 -0600

    Finished Alert Page and Avatar Page from videos 76 & 77

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class AvatarPage extends StatelessWidget {
@@ -6,8 +5,31 @@ class AvatarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Avatar Page'),
-      )
+        title: Text('Avatar Page'), 
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child:CircleAvatar(
+              backgroundImage: NetworkImage("https://p16.muscdn.com/img/musically-maliva-obj/1631892931579910~c5_1080x1080.jpeg"),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child:CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            )
+          )
+        ]
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage("https://i.pinimg.com/originals/11/9e/3a/119e3a4678cb2d80b61f07118ed37ac4.jpg"),
+          placeholder: AssetImage("assets/jar-loading.gif"),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+      ),
     );
   }
 }
